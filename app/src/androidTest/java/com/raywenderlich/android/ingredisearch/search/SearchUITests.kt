3b721a935f2aeb3dd123fdbe05dc25e7039c5e92
Aleksandr Levenko <aleksandr.levenko@itomy.ch>
2019-05-28 15:03:18 +0300

    4 test with result

--- app/src/androidTest/java/com/raywenderlich/android/ingredisearch/search/SearchUITests.kt
+++ app/src/androidTest/java/com/raywenderlich/android/ingredisearch/search/SearchUITests.kt
@@ -1,5 +1,7 @@
 package com.raywenderlich.android.ingredisearch.search
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.filters.LargeTest
 import com.agoda.kakao.common.views.KView
@@ -8,6 +10,7 @@ import com.agoda.kakao.intent.KIntent
 import com.agoda.kakao.screen.Screen
 import com.agoda.kakao.text.KButton
 import com.raywenderlich.android.ingredisearch.R
+import com.raywenderlich.android.ingredisearch.recentIngredients.RecentIngredientsActivity
 import com.raywenderlich.android.ingredisearch.searchResults.SearchResultsActivity
 import org.junit.Rule
 import org.junit.Test
@@ -18,6 +21,7 @@ class SearchScreen : Screen<SearchScreen>() {
         withId(com.google.android.material.R.id.snackbar_text)
     }
     val ingredients = KEditText { withId(R.id.ingredients) }
+    val recentButton = KView { withId(R.id.recentButton) }
 
 }
 
@@ -69,4 +73,32 @@ class SearchUITests {
             searchResultsIntent.intended()
         }
     }
+
+    @Test
+    fun choosingRecentIngredients_shouldSetCommaSeparatedIngredients() {
+        screen {
+            // 1
+            val recentIngredientsIntent = KIntent {
+                hasComponent(RecentIngredientsActivity::class.java.name)
+                withResult {
+                    withCode(RESULT_OK)
+                    withData(
+                        Intent().putStringArrayListExtra(
+                            RecentIngredientsActivity.EXTRA_INGREDIENTS_SELECTED,
+                            ArrayList(listOf("eggz", "onionz"))
+                        )
+                    )
+                }
+            }
+            // 2
+            recentIngredientsIntent.intending()
+
+            // 3
+            recentButton.click()
+
+            // 4
+            ingredients.hasText("eggz,onionz")
+        }
+    }
+
 }
